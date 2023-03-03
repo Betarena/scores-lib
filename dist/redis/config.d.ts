786133993890809606a9d613a0036b6140a5e624
migbash <20924663+migbash@users.noreply.github.com>
2023-03-03 20:11:41 +0000

    git commit -m 'Removed files in the .gitignore'

--- dist/redis/config.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const LS2_C_D_A = "livescores_v2_data";
-export declare const LS2_C_T_A = "livescores_v2_tran";
-export declare const LS2_C_S_A = "livescores_v2_seo";
-export declare const FO_C_D_A = "tour_fix_odds_data";
-export declare const FO_C_T_A = "tour_fix_odds_t";

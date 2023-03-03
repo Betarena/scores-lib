08ff2b5732dd1d578683f393fe65bc3256be356b
migbash <20924663+migbash@users.noreply.github.com>
2023-03-03 05:43:26 +0000

    feat: redis/config.ts (up);

--- src/redis/config.ts
+++ src/redis/config.ts
@@ -2,3 +2,6 @@
 export const LS2_C_D_A = 'livescores_v2_data';
 export const LS2_C_T_A = 'livescores_v2_tran';
 export const LS2_C_S_A = 'livescores_v2_seo';
+// [ℹ] tournament (league) page
+export const FO_C_D_A = "tour_fix_odds_data"
+export const FO_C_T_A = "tour_fix_odds_t"
